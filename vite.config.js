238a8de2a2e34415960421cd81c731cc6014e5f4
Shariful Islam <sharifulislam242248@gmail.com>
2026-02-05 15:24:46 +0600

    done

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 @import "tailwindcss";
 
 @theme {
-  /* কাস্টম স্লো পালস অ্যানিমেশন */
   --animate-pulse-slow: pulse 6s cubic-bezier(0.4, 0, 0.6, 1) infinite;
-  
-  /* কাস্টম স্লো বাউন্স অ্যানিমেশন */
   --animate-bounce-slow: bounce 3s infinite;
 }
 
